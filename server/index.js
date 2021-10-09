5645d8633f56ffdfffc99793a11a56d4bb1f126d
Sher Shah Arsalaie <shershah.arsalaie@hotmail.com>
2021-10-08 19:55:04 -0700

    running server and adding some fake data

--- server/index.js
+++ server/index.js
@@ -1,6 +1,40 @@
 const express = require('express')
+const {graphqlHTTP} = require('express-graphql')
+
 const app = express()
-const port = 5000;
+const port = 8000;
+
+const todo = [
+  {
+    id: 1,
+    name: 'Hiking',
+    ownerId: 1,
+    description: 'Capilano Hills'
+  },
+  {
+    id: 2,
+    name: 'Biking',
+    ownerId: 2,
+    description: 'Canada Place'
+  },
+  {
+    id: 3,
+    name: 'Coffee',
+    ownerId: 3,
+    description: 'Starbucks'
+  },
+  {
+    id: 4,
+    name: 'Picnic',
+    ownerId: 4,
+    description: 'Sasamat Lake'
+  }
+]
+
+app.use('/graphql', graphqlHTTP({
+  graphiql: true,
+  schema: schema
+}))
 
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
